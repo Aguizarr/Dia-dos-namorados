5f7a76af033af677762b6c1af0c833c03bf305b3
Aguizarr <128240443+Aguizarr@users.noreply.github.com>
2025-06-09 15:40:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,32 +22,32 @@ function validarLogin() {
           <div class="foto-conquista">
             <img src="foto1.jpg" alt="Foto 1">
             <div class="legenda">Essas fotos são poucas comparado ao meu sentimento por você vida😍</div>
-            <div class="coracao-animado"></div>
+            <div class="coracao-animado">❤️</div>
           </div>
           <div class="foto-conquista">
             <img src="foto2.jpg" alt="Foto 2">
             <div class="legenda">Esse foi o dia em que fomos no parque SJP de noite, fomos so pra comer milho e passar frio kkkk💘</div>
-            <div class="coracao-animado"></div>
+            <div class="coracao-animado">❤️</div>
           </div>
           <div class="foto-conquista">
             <img src="foto3.jpg" alt="Foto 3">
             <div class="legenda">O dia que fomos passar um tempo com a sua família no parque Naútico❤️</div>
-            <div class="coracao-animado"></div>
+            <div class="coracao-animado">❤️</div>
           </div>
           <div class="foto-conquista">
             <img src="foto4.jpg" alt="Foto 4">
             <div class="legenda">Teu sorriso ilumina meu mundo ❤️</div>
-            <div class="coracao-animado"></div>
+            <div class="coracao-animado">❤️</div>
           </div>
           <div class="foto-conquista">
             <img src="foto5.jpg" alt="Foto 5">
             <div class="legenda"> Sua chatisse me estressa, mas não vivo sem hehe ❤️</div>
-            <div class="coracao-animado"></div>
+            <div class="coracao-animado">❤️</div>
           </div>
           <div class="foto-conquista">
             <img src="foto6.jpg" alt="Foto 6">
             <div class="legenda"> Meu ❤️ Te amo!! ❤️ </div>
-            <div class="coracao-animado"></div>
+            <div class="coracao-animado">❤️</div>
           </div>
         </div>
       </div>
