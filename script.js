8d6ff560ebd55497946b3f02fe7ff772523114b3
Aguizarr <128240443+Aguizarr@users.noreply.github.com>
2025-06-09 18:53:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function validarLogin() {
   
   function mostrarMensagem() {
     document.body.innerHTML = `
-      <div id="conteudo" class="container">
+      <div class="container">
         <h1>💖 Bem-vinda, meu amor! 💖</h1>
         <p>Eu preparei isso com muito carinho. Passe o mouse nas fotos meu amor!</p>
         <div class="galeria">
